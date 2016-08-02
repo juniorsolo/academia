f46baf75920a178cba6334dafa529560111ca6e0
Manoel <manoel.silva@verity.com>
2016-08-02 17:14:29 -0300

    Experiência com o include de páginas.

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){ 
+   $("#enviar").click( function(){
+      $("#footer").load("footer.html");
+   });
+   $("#novo").click( function(){
+      $("#footer").load("footer2.html");
+   });
+}); 
+ 
