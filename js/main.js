cc3b7fa573cb951e203a7f664bb0e720c57f9070
Manoel <manoel.silva@verity.com>
2016-08-03 17:36:20 -0300

    Adicionando template de menu e quadro de execercicios.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,115 @@
 $(document).ready(function(){ 
-   $("#enviar").click( function(){
-      $("#footer").load("footer.html");
+   $("#Biceps").click( function(){
+      //Exibindo...
+	  $("#BICEPS").fadeIn('slow');
+	  
+	  //Ocultando outros
+	  $("#TRICEPS").fadeOut('fast');
+	  $("#COSTAS").fadeOut('fast');
+	  $("#PEITORAL").fadeOut('fast');
+	  $("#PERNAS").fadeOut('fast');
+	  $("#TRAPEZIO").fadeOut('fast');
+	  $("#DELTOIDE").fadeOut('fast');
+	  $("#GLUTEO").fadeOut('fast');
    });
-   $("#novo").click( function(){
-      $("#footer").load("footer2.html");
+   
+   $("#Triceps").click(function(){
+	   // Exibindo...
+	  $("#TRICEPS").fadeIn('slow');
+	  
+	   //Ocultando outros
+	  $("#BICEPS").fadeOut('fast');
+	  $("#COSTAS").fadeOut('fast');
+	  $("#PEITORAL").fadeOut('fast');
+	  $("#PERNAS").fadeOut('fast');
+      $("#TRAPEZIO").fadeOut('fast');
+	  $("#DELTOIDE").fadeOut('fast');
+	  $("#GLUTEO").fadeOut('fast');	  
    });
+   
+   $("#Costas").click(function(){
+	   // Exibindo...
+	  $("#COSTAS").fadeIn('slow');
+	  
+	   //Ocultando outros
+	  $("#BICEPS").fadeOut('fast');
+	  $("#TRICEPS").fadeOut('fast');
+	  $("#PEITORAL").fadeOut('fast');
+	  $("#PERNAS").fadeOut('fast');
+	  $("#TRAPEZIO").fadeOut('fast');
+	  $("#DELTOIDE").fadeOut('fast');
+	  $("#GLUTEO").fadeOut('fast');
+   });
+   
+   $("#Peitoral").click(function(){
+	   // Exibindo...
+	  $("#PEITORAL").fadeIn('slow');
+	  
+	   //Ocultando outros
+	  $("#BICEPS").fadeOut('fast');
+	  $("#TRICEPS").fadeOut('fast');
+	  $("#COSTAS").fadeOut('fast');
+	  $("#PERNAS").fadeOut('fast');
+	  $("#TRAPEZIO").fadeOut('fast');
+	  $("#DELTOIDE").fadeOut('fast');
+	  $("#GLUTEO").fadeOut('fast');
+   });
+   
+   $("#Pernas").click(function(){
+	   // Exibindo...
+	  $("#PERNAS").fadeIn('slow');
+	  
+	   //Ocultando outros
+	  $("#BICEPS").fadeOut('fast');
+	  $("#TRICEPS").fadeOut('fast');
+	  $("#COSTAS").fadeOut('fast');
+	  $("#PEITORAL").fadeOut('fast');
+	  $("#TRAPEZIO").fadeOut('fast');
+	  $("#DELTOIDE").fadeOut('fast');
+	  $("#GLUTEO").fadeOut('fast'); 
+   });
+   
+   $("#Trapezio").click(function(){
+	   // Exibindo...
+	  $("#TRAPEZIO").fadeIn('slow');
+	  
+	   //Ocultando outros
+	  $("#BICEPS").fadeOut('fast');
+	  $("#TRICEPS").fadeOut('fast');
+	  $("#COSTAS").fadeOut('fast');
+	  $("#PEITORAL").fadeOut('fast');
+	  $("#PERNAS").fadeOut('fast');
+	  $("#DELTOIDE").fadeOut('fast');
+	  $("#GLUTEO").fadeOut('fast'); 
+   });
+   
+   $("#Deltoide").click(function(){
+	   // Exibindo...
+	  $("#DELTOIDE").fadeIn('slow');
+	  
+	   //Ocultando outros
+	  $("#BICEPS").fadeOut('fast');
+	  $("#TRICEPS").fadeOut('fast');
+	  $("#COSTAS").fadeOut('fast');
+	  $("#PEITORAL").fadeOut('fast');
+	  $("#PERNAS").fadeOut('fast');
+	  $("#TRAPEZIO").fadeOut('fast');
+	  $("#GLUTEO").fadeOut('fast'); 
+   });
+   
+   $("#Gluteo").click(function(){
+	   // Exibindo...
+	  $("#GLUTEO").fadeIn('slow');
+	  
+	   //Ocultando outros
+	  $("#BICEPS").fadeOut('fast');
+	  $("#TRICEPS").fadeOut('fast');
+	  $("#COSTAS").fadeOut('fast');
+	  $("#PEITORAL").fadeOut('fast');
+	  $("#PERNAS").fadeOut('fast');
+	  $("#TRAPEZIO").fadeOut('fast');
+	  $("#DELTOIDE").fadeOut('fast'); 
+   });
+   
 }); 
  
